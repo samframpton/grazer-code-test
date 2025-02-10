f813ba090df35c8f07325d95db54afa5e8e8d3d2
Sam Frampton <91746301+samframpton@users.noreply.github.com>
2025-02-10 17:15:16 +0000

    Add hilt dependencies

---
+++ app/src/main/java/sam/frampton/grazer/GrazerApplication.kt
@@ -0,0 +1,7 @@
+package sam.frampton.grazer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GrazerApplication : Application()
--- app/src/main/java/sam/frampton/grazer/MainActivity.kt
+++ app/src/main/java/sam/frampton/grazer/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import sam.frampton.grazer.ui.theme.GrazerCodeTestTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
